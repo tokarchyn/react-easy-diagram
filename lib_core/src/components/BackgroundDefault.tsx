b537c302c1a2aef72425165131db78bc17587b33
tokarchyn <tokarchyn@gmail.com>
2021-03-20 16:17:50 +0100

    Add background configuration. Add dots background

--- lib_core/src/components/BackgroundDefault.tsx
+++ lib_core/src/components/BackgroundDefault.tsx
@@ -1,40 +1,104 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { IComponentDefinition } from '..';
 import { IBackgroundComponentProps } from '../states/diagramSettings';
 
 const BackgroundDefault: React.FC<
   IBackgroundComponentProps<IBackgroundDefaultSettings>
-> = ({ diagramOffset, diagramZoom: digramZoom, settings }) => {
-  settings = settings ?? defaultSettings;
+> = ({ diagramOffset, diagramZoom, settings }) => {
+  const finalSettings = settings ?? defaultSettings;
+
+  const backgroundImage = useMemo(
+    () =>
+      finalSettings.imageGenerator
+        ? finalSettings.imageGenerator(100 * diagramZoom, 100 * diagramZoom)
+        : undefined,
+    [finalSettings, finalSettings.imageGenerator, diagramZoom]
+  );
 
   return (
     <div
       className='react_fast_diagram_Background_Default'
       style={{
-        backgroundColor: settings.color,
-        backgroundImage: settings.imageCreator(
-          100 * digramZoom,
-          100 * digramZoom
-        ),
+        backgroundColor: finalSettings.color,
+        backgroundImage,
         backgroundPosition: `${diagramOffset[0]}px ${diagramOffset[1]}px`,
       }}
     />
   );
 };
 
-export const createGridImage: ImageCreator = (width, height) => {
-  return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${width}' height='${height}' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.3'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`;
+const gridImageGenerator = (
+  width: number,
+  height: number,
+  sizeMultiplicator: number,
+  linesColor: string,
+  linesOpacity: number
+) => {
+  linesColor = linesColor.replace('#', '%23');
+  return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${
+    width * sizeMultiplicator
+  }' height='${
+    height * sizeMultiplicator
+  }' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='${linesColor}' fill-opacity='${linesOpacity}'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`;
 };
 
-export const createImageWithCrosses: ImageCreator = (width, height) => {
+export const createGridImageGenerator = (
+  sizeMultiplicator: number,
+  linesColor: string,
+  linesOpacity: number
+): BackgroundImageGenerator => (width: number, height: number) =>
+  gridImageGenerator(
+    width,
+    height,
+    sizeMultiplicator,
+    linesColor,
+    linesOpacity
+  );
+
+const dotsImageGenerator = (
+  width: number,
+  height: number,
+  sizeMultiplicator: number,
+  dotsColor: string,
+  dotsOpacity: number,
+  dotsRadius: number
+) => {
+  sizeMultiplicator = 0.1 * sizeMultiplicator;
+  dotsColor = dotsColor.replace('#', '%23');
+  return `url("data:image/svg+xml,%3Csvg width='${
+    width * sizeMultiplicator
+  }' height='${
+    height * sizeMultiplicator
+  }' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='${dotsColor}' fill-opacity='${dotsOpacity}' fill-rule='evenodd'%3E%3Ccircle cx='3' cy='3' r='${dotsRadius}'/%3E%3Ccircle cx='13' cy='13' r='${dotsRadius}'/%3E%3C/g%3E%3C/svg%3E")`;
+};
+
+export const createDotsImageGenerator = (
+  sizeMultiplicator: number,
+  dotsColor: string,
+  dotsOpacity: number,
+  dotsRadius: number
+): BackgroundImageGenerator => (width: number, height: number) =>
+  dotsImageGenerator(
+    width,
+    height,
+    sizeMultiplicator,
+    dotsColor,
+    dotsOpacity,
+    dotsRadius
+  );
+
+export const crossesImageGenerator: BackgroundImageGenerator = (
+  width,
+  height
+) => {
   return `url("data:image/svg+xml,%3Csvg width='${width * 0.1}' height='${
     height * 0.1
   }' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23cacaca' fill-opacity='0.3'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`;
 };
 
 const defaultSettings: IBackgroundDefaultSettings = {
-  imageCreator: createImageWithCrosses,
-  color: '#ffffff',
+  imageGenerator: createGridImageGenerator(1, '#858585', 0.1),
+  color: '#eeeeee',
 };
 
 export const createDefaultBackground = (
@@ -54,14 +118,17 @@ export const createDefaultBackground = (
   };
 };
 
-export type ImageCreator = (width: number, height: number) => string;
+export type BackgroundImageGenerator = (
+  width: number,
+  height: number
+) => string;
 
 /**
- * @property {function}  imageCreator       - Function to create string for css's backgroundUrl property.
+ * @property {function}  imageGenerator - Function to create string for css's backgroundUrl property.
  * You can use for example services like listed in this article https://css-tricks.com/websites-generate-svg-patterns/
  * to generate this string.
  */
 export interface IBackgroundDefaultSettings {
-  imageCreator: ImageCreator;
+  imageGenerator?: BackgroundImageGenerator;
   color: string;
 }
