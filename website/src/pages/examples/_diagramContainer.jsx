83e6946d33d05b967e485845fae582836e2dcac3
tokarchyn <tokarchyn@gmail.com>
2021-05-03 00:00:58 +0200

    Fix SSR turning off for diagrams example

--- website/src/pages/examples/_diagramContainer.jsx
+++ website/src/pages/examples/_diagramContainer.jsx
@@ -3,12 +3,8 @@ import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export function DiagramContainer(props) {
   return (
-    <BrowserOnly>
-      {() => (
-        <div style={{ height: 'calc(90vh - var(--ifm-navbar-height))' }}>
-          {props.children}
-        </div>
-      )}
-    </BrowserOnly>
+    <div style={{ height: 'calc(90vh - var(--ifm-navbar-height))' }}>
+      <BrowserOnly>{() => props.children}</BrowserOnly>
+    </div>
   );
 }
